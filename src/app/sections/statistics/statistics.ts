13d5f8ccf83078c346812a9c4b059550d939509d
Brett Mitchell <brightpixelstudios@gmail.com>
2026-09-21 10:31:27 -0400

    Updates

--- src/app/sections/statistics/statistics.ts
+++ src/app/sections/statistics/statistics.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit, ChangeDetectorRef, Inject } from '@angular/core';
 import { ApiServiceStatistics } from '../../services/statisticservice';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'statistics',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './statistics.html',
   styleUrl: './statistics.css',
 })
 export class Statistics implements OnInit {
+  getStatisticsInformation: any = '';
+
   constructor(
     @Inject(ApiServiceStatistics) private apiService: ApiServiceStatistics,
     private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
-    //this.loadStatistics();
+    this.loadStatistics();
+  }
+
+  private loadStatistics(): void {
+    // get ALL Blog information
+    this.apiService.getStatistics().subscribe({
+      next: (data) => {
+        // Data maps exactly to the keys defined in forkJoin
+        this.getStatisticsInformation = data;
+        console.log(this.getStatisticsInformation);
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.cdr.detectChanges();
+        console.error('One or more requests failed:', err);
+      },
+    });
   }
 }
