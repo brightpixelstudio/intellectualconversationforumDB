2da0d3fc451a749d2b8e54c146c6acc7a8f3c217
Brett Mitchell <brightpixelstudios@gmail.com>
2026-09-22 02:32:08 -0400

    Updates

--- src/app/services/postservice.ts
+++ src/app/services/postservice.ts
@@ -2,6 +2,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, forkJoin } from 'rxjs';
+import { GetPostsByCategoryUser } from '../models/member/getpostsbycategoryuser';
 
 @Injectable({
   providedIn: 'root', // Makes the service a global singleton
